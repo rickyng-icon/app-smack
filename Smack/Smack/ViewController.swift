d8b99e994644ce6563fa5042ec8c1d863bf7d0fd
rickyng-icon <rickyng@icon-info.com.hk>
2017-09-06 17:34:12 +0800

    Starter

---
+++ Smack/Smack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Ng Ricky on 6/9/2017.
+//  Copyright © 2017 ICON. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
